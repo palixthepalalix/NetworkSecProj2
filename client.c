7b19c6ef465685dd204ef784eb954dc2e60c548b
Alix <alix.cook11@gmail.com>
2015-03-12 13:10:23 -0400

    setup all there, now just have to set up tcp connection for communication

--- client.c
+++ client.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "libs/hash.h"
+#include "hash.h"
 #include "libs/aes.h"
 
+#define DISK_IO_BUF_SIZE 4096
+
+static unsigned char *iv = "01234567890123456"; // make sure this is the right length?
 void handlePutRequest(char *fileName, int encrypted, char *pswd);
 void handleGetRequest(char *fileName, int encrypted, char *pswd);
 
@@ -77,13 +80,62 @@ void parseRequest(char *request)
         handleGetRequest(requestFile, encrypted, pswd);
 }
 
+void makeKey(char *pswd, char *buf)
+{
+    strcpy(buf, "1234567890123456");
+}
+
 void handlePutRequest(char *fileName, int encrypted, char *pswd)
 {
+    //generate SHA256 hash of plaintext file
+
     printf("handling put request\n");
+    unsigned char *ftext, hashBuff[2048/8];
+    char *ciphertext, buf[2000], key[16];
+    int ciphLen, fsize;
+    //make sure handling different directories
+    //and handling binary and ascii files
+    FILE *f;
+    f = fopen(fileName, "rb");
+    if(f == NULL) {
+        printError("File cannot be transferred");
+        return;
+    }
+    int n;
+    fseek(f, 0, SEEK_END);
+    fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    ftext = malloc(fsize + 1);
+    fread(ftext, fsize, 1, f);
+    fclose(f);
+    printf("%s", ftext);
+    //can only use hash on clic machines
+    //hash(buf, hashBuff);
+
+    //if encrypted, use password as seed to random number generator
+ 
+    if(encrypted) {
+        //encrypt this shit
+        makeKey(pswd, key);
+        ciphertext = malloc(fsize + 256); // size of file plus aes block size
+        //gotta prepend that mutha fuggin IV
+        //ciphLen = aes(key, ftext, strlen(buf), ciphertext, 1);
+    }
+    //send file (with iv prepended if valid) and hash to server
+    //send(sock, ciphertext, ciphLen, 0);
+    //send(sock, hashBuff, sizeof(hashBuff), 0);
 }
 
 void handleGetRequest(char *fileName, int encrypted, char *pswd)
 {
+
+    //recv file
+    //decrypt if need be
+    //compute hash
+    //hash(txt, buf)
+    //compare
+    //if valid, write that shit to directory
+    //not valid, display msg to user
     printf("handling get request\n");
 }
 
